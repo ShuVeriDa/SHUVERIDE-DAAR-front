4684f45c36aa259fa9fe12d4e8a51101ee94be92
ShuVeriDa <bashtarov@outlook.com>
2023-02-25 10:05:19 +0300

    added to blacklist comment and food

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -7,7 +7,7 @@ import {rootReducer} from "./reducers";
 const persistConfig = {
   key: 'root',
   storage,
-  blacklist: ['filter', 'user']
+  blacklist: ['filter', 'user', 'comment', 'food']
 }
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
